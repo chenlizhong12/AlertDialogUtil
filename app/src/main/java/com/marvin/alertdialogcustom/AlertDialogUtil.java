e1ba08406ed22d38e4a0ae16e374bda5164d6b59
chenlizhong12 <472874481@qq.com>
2019-07-19 16:28:59 +0800

    alertToast

--- app/src/main/java/com/marvin/alertdialogcustom/AlertDialogUtil.java
+++ app/src/main/java/com/marvin/alertdialogcustom/AlertDialogUtil.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -30,16 +31,17 @@ import android.widget.TextView;
  * Created by clz on 2019/7/19
  */
 public class AlertDialogUtil {
-    public static void showAlertDialog(Context context, String title, final TextView tv, final String[] args){
+    private  OnDialogListener mOnDialogListener;
+
+    public static void showAlertDialog(Context context, String title, final TextView tv, final String[] args) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         final AlertDialog dialog = builder.create();
         dialog.show();
-
         View view = LayoutInflater.from(context).inflate(R.layout.dialog_view, null);
         TextView tvTitle = (TextView) view.findViewById(R.id.tv_title_alert_dialog_addbook);
-        ListView list = (ListView)view.findViewById(R.id.lv_alert_dialog_addbook);
+        ListView list = (ListView) view.findViewById(R.id.lv_alert_dialog_addbook);
         tvTitle.setText(title);
-        ListAdapter adpter = new ArrayAdapter<String>(context,R.layout.item_dialog_view,R.id.tv_item_listview_categroy,args);
+        ListAdapter adpter = new ArrayAdapter<String>(context, R.layout.item_dialog_view, R.id.tv_item_listview_categroy, args);
         list.setAdapter(adpter);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -48,9 +50,48 @@ public class AlertDialogUtil {
                 dialog.dismiss();
             }
         });
-        WindowManager.LayoutParams  lp= dialog.getWindow().getAttributes();
+        WindowManager.LayoutParams lp = dialog.getWindow().getAttributes();
         lp.width = context.getResources().getDimensionPixelSize(R.dimen.dialog_width);
         dialog.getWindow().setAttributes(lp);
         dialog.getWindow().setContentView(view);
+    }
+
+    public  void showAlertDialogToast(Context context, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        Window window = alertDialog.getWindow();
+        WindowManager.LayoutParams lp = alertDialog.getWindow().getAttributes();
+        lp.width = context.getResources().getDimensionPixelSize(R.dimen.dialog_width);
+        alertDialog.getWindow().setAttributes(lp);
+        window.setContentView(R.layout.dialog_toast);
+        TextView tv_dialog = window.findViewById(R.id.dialog_text);
+        tv_dialog.setText(message);
+        TextView confirm = window.findViewById(R.id.confirm);
+        TextView cancel = window.findViewById(R.id.cancel);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mOnDialogListener.onConfirmClick();
+                alertDialog.dismiss();
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 mOnDialogListener.onCancelClick();
+                alertDialog.dismiss();
+            }
+        });
+    }
+
+    public  void setOnDialogListener(OnDialogListener onDialogListener) {
+        this.mOnDialogListener = onDialogListener;
+    }
+
+    public interface OnDialogListener {
+        void onConfirmClick();
+
+        void onCancelClick();
     }
 }
--- app/src/main/java/com/marvin/alertdialogcustom/MainActivity.java
+++ app/src/main/java/com/marvin/alertdialogcustom/MainActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mDialog_button;
     private final String[] Category = new String[]{  "分享", "编辑", "删除", "取消"};
     private TextView mTv_dialog;
+    private Button mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initView() {
         mDialog_button = findViewById(R.id.dialog_button);
         mTv_dialog = findViewById(R.id.tv_dialog);
+        mToast = findViewById(R.id.toast);
+        mToast.setOnClickListener(this);
         mDialog_button.setOnClickListener(this);
     }
 
@@ -32,6 +36,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.dialog_button:
                 AlertDialogUtil.showAlertDialog(this,"更多操作",mTv_dialog,Category);
                 break;
+            case R.id.toast:
+                AlertDialogUtil alertDialogUtil = new AlertDialogUtil();
+                alertDialogUtil.showAlertDialogToast(this, "确定删除这条记录");
+                alertDialogUtil.setOnDialogListener(new AlertDialogUtil.OnDialogListener() {
+                    @Override
+                    public void onConfirmClick() {
+                        Toast.makeText(MainActivity.this, "确定", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelClick() {
+
+                    }
+                });
         }
     }
 }
